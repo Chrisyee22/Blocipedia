099c751a45f2e08133260caa3dcd6b09d413d5bf
Chris <chris.anthony.yee@gmail.com>
2018-02-11 13:43:27 -0800

    to be deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  resources :wikis
+
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   get 'welcome/index'
 
   get 'welcome/about'
