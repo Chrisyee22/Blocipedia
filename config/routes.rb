6838564dc344d61b5e2f1f1463f757a3396c1f68
Chris <chris.anthony.yee@gmail.com>
2018-02-11 19:58:09 -0800

    added CRUD and updated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'wikis/index'
 
-  get 'wikis/show'
-
-  get 'wikis/new'
-
-  get 'wikis/edit'
+  resources :wikis
 
   devise_for :users
   get 'welcome/index'
