d6796e16f19581900309bd959e017fd0ab117d5a
Chris <chris.anthony.yee@gmail.com>
2018-02-14 16:49:08 -0800

    user roles

--- Gemfile
+++ Gemfile
@@ -46,3 +46,4 @@ group :development, :test do
 
 gem 'bootstrap-sass'
 gem 'devise'
+gem 'pundit'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save { self.email = email.downcase }
+  before_save { self.role ||= :standard }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :wiki
+
+  enum role: [:standard, :premium, :admin]
 
   attr_accessor :login
 
